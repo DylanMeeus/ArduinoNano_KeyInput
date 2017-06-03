cc99b434859317bfbcbfca0916e81637b7e1d4ed
Dylan Meeus <meeusdylan@hotmail.com>
2017-06-03 18:12:22 +0200

    One event per press

--- NanoCode_pde/NanoCode_pde.ino
+++ NanoCode_pde/NanoCode_pde.ino
@@ -1,14 +1,32 @@
 
+/**
+  Code to make the keyswitch tester come alive
+*/
 
+
+const int button = 2;
+
+int buttonState = 0;
+boolean wasPressed = false;
 
 void setup(){
   
   Serial.begin(9600);
-  
+  pinMode(button,INPUT);
+      
 }
 
 void loop(){
   
-  Serial.println("up");
   
+  buttonState = digitalRead(button);
+  if(buttonState == HIGH){
+    if(wasPressed == false){
+      Serial.println("up");
+      wasPressed = true;
+    }
+  } else {
+//    Serial.println("down");
+    wasPressed = false;
+  }  
 }
